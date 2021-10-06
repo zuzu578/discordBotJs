3b6f0237666d91be5cb7e2ff00776c8aeee23ea6
zuzu <dlwnghks6821@naver.com>
2021-10-06 10:02:53 +0900

    commit on master

--- DiscordBot.js
+++ DiscordBot.js
@@ -1,20 +1,27 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('hello world');
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const request = require("request");
+client.on("ready", () => {
+  console.log(`${client.user.tag}에 로그인하였습니다!`);
+  
 });
-
-client.on('interactionCreate', async interaction => {
-
-
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+//사용자가 채팅을 입력할때 실행되는 function 
+client.on("message", msg => {
+ console.log(msg);
+  let content = "";
+ 
+  if (msg.content == "!youtube") {
+    //content = msg.content.split("");
+    msg.reply("https://www.youtube.com");
   }
-
+  
+ 
+ 
+ 
+   
+  
+  
 });
+//client.login("토큰");
 
-client.login('ODk0NTgwMTc0MDg2MDgyNTgw.YVsEtQ.UaXd2LF2Pg0XBb-okGSa1m3PwyA');
+client.login('ODk0NzM5Mzg5Nzk0NjQ0MTE4.YVuY_Q.ZWBi8qyn28g89wSKXdGgvsac5-g');
